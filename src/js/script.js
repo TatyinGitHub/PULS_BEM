2c812b8fa02630d7fcdf2faf8a3c2fb1a67130cc
Taty <t0637889337@gmail.com>
2022-11-10 18:31:44 +0200

    _modal

--- src/js/script.js
+++ src/js/script.js
@@ -66,5 +66,30 @@ $(document).ready(function(){
       $('.overlay, #order').fadeIn('slow');
     })
   });
+  
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Пожалуйста, введите свое имя",
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свой почтовый адрес",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  valideForms('#consultation-form');
+  valideForms('#consultation form');
+  valideForms('#order form');
   });
   
